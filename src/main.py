a78a1db2b989f28277ac42e74d37f21a1c6be8f6
snugghash <snugghash@gmail.com>
2017-03-19 22:01:20 -0400

    Python, writing into ICS file

---
+++ src/main.py
@@ -0,0 +1,28 @@
+from icalendar import Calendar, Event
+from datetime
+from pytz import UTC # timezone
+import dateutil.parser # Hacky format guesser, returning datetime objects
+
+
+
+"""
+Assumptions:
+Duration is in minutes
+"""
+def iCal_from_times_list(times_list, project_name, duration, priority=5):
+    cal = Calendar()
+    cal.add('prodid', 'cranki')
+    cal.add('version', '0.1')
+    for count,time in enumerate(times_list):
+        event = Event()
+        event.add('summary', project_name)
+        event.add('dtstart', dateutil.parser(time))
+        event.add('dtend', dateutil.parser(time) + datetime.timedelta(0,0,0,0,60))
+        event.add('dtstamp', datetime.datetime.now())
+        event['uid'] = dateutil.parser(time).isoformat() + '@' + project_name + '/cranki'
+        event.add('priority', priority)
+        cal.add_component(event) 
+
+    f = open('schedule.ics', 'wb')
+    f.write(cal.to_ical())
+    f.close()
